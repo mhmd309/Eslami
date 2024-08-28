88be853aebd93c409e14f2cf7e39c785f6707b23
Mohamed Elseedi <81447775+mhmd309@users.noreply.github.com>
2024-08-28 19:08:20 +0300

    Add files via upload

---
+++ js/custome.js
@@ -0,0 +1,62 @@
+$(function () {
+  $('.quantity .number').click(function () {
+    var personIndex = $(this).index(),
+      personThis = $(this).parent().find('input'),
+      personCount = personThis.val(),
+      personMin = personThis.attr('data-min');
+    if (personIndex == 0) {
+      personCount = Number(personCount) - Number(1);
+      if (personCount >= personMin) {
+        $(this).parent().find('input').val(personCount);
+      }
+    }
+    if (personCount == "0") {
+      personThis.addClass('active').fadeOut(1500);
+      $(this).fadeOut(500);
+    }
+  });
+});
+// Quran Kareem
+let surahsContainer = document.querySelector('.name-surah');
+getSurahs();
+function getSurahs() {
+  // fetch surahs names
+  fetch("http://api.alquran.cloud/v1/meta")
+    .then(Response => Response.json())
+    .then(data => {
+      let surahs = data.data.surahs.references;
+      let numberOfSurahs = 114;
+      surahsContainer.innerHTML = "";
+      for (i = 0; i < numberOfSurahs; i++) {
+        surahsContainer.innerHTML += `
+        <div class="surah m-2">
+          <p class="m-0 mb-2">${surahs[i].number}</p>
+          <p class="m-0 mb-2">${surahs[i].name}</p>
+          <p class="m-0">${surahs[i].englishName}</p>
+        </div>`
+      }
+      let surahsTitles = document.querySelectorAll('.surah');
+      let popup = document.querySelector('.surah-pop');
+      let ayatContiner = document.querySelector('.ayat');
+      surahsTitles.forEach((title, index) => {
+        title.addEventListener('click', () => {
+          fetch(`http://api.alquran.cloud/v1/surah/${index + 1}`)
+            .then(Response => Response.json())
+            .then(data => {
+              ayatContiner.innerHTML = "";
+              let ayat = data.data.ayahs;
+              ayat.forEach(aya => {
+                popup.classList.add('active');
+                ayatContiner.innerHTML += `
+                  <p class="m-0">${aya.text} - (${aya.numberInSurah})</p>
+                `
+              })
+            })
+        })
+      })
+      let closePopup = document.querySelector('.close-pop');
+      closePopup.addEventListener('click', () => {
+        popup.classList.remove('active');
+      })
+    })
+}
